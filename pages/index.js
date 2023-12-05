45f3e0d56a807dbbd4e7bc67d1227b02cb58de96
rrcjade <rrcjadework@gmail.com>
2023-12-05 18:35:12 -0500

    set itemsperpage to 50

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const IndexPage = () => {
   const [products, setProducts] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPage, setTotalPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 50;
   const [loaded, setLoaded] = useState(false);
   const scrollToTop = () => {
     window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
